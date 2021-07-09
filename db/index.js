19e0fb9b85689cc4b9f2832b1f0da4a41711c275
Logan Duarte <loganeduarte@hotmail.com>
2021-07-08 20:11:27 -0700

    restructure project

--- db/index.js
+++ db/index.js
@@ -1,40 +1,52 @@
-// Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'linkerator'
-const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
-const client = new Client(DB_URL);
+// // Connect to DB
+// const { Client } = require('pg');
+// const DB_NAME = 'linkerator'
+// const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
+// const client = new Client(DB_URL);
 
-// database methods
-const insertLink = async (link_name) => {
-  const { rows } = await client.query(`
-    INSERT INTO links (link_name) VALUES ($1) RETURNING id;
-  `, [link_name]);
+// // database methods
+// const insertLink = async (link_name) => {
+//   const { rows } = await client.query(`
+//     INSERT INTO links (link_name) VALUES ($1) RETURNING id;
+//   `, [link_name]);
 
-  return rows[0].id;
-};
+//   return rows[0].id;
+// };
 
-const insertTag = async (tag_name) => {
-  const { rows } = await client.query(`
-  INSERT INTO tags (tag_name) VALUES ($1) RETURNING id;
-  `, [tag_name]);
+// const insertTag = async (tag_name) => {
+//   const { rows } = await client.query(`
+//   INSERT INTO tags (tag_name) VALUES ($1) RETURNING id;
+//   `, [tag_name]);
 
-return rows[0].id;
-};
+// return rows[0].id;
+// };
 
-const insertLinkTag = async (linksId, tagsId) => {
-  const { rows } = await client.query(`
-  INSERT INTO links_tags ("linksId", "tagsId")
-  VALUES ($1, $2)
-  RETURNING id;
-  `, [linksId, tagsId]);
+// const insertLinkTag = async (linksId, tagsId) => {
+//   const { rows } = await client.query(`
+//   INSERT INTO links_tags ("linksId", "tagsId")
+//   VALUES ($1, $2)
+//   RETURNING id;
+//   `, [linksId, tagsId]);
 
-  return rows[0].id;
-};
+//   return rows[0].id;
+// };
 
-// export
-module.exports = {
-  client,
-  insertLink,
-  insertTag,
-  insertLinkTag,
+// const getTagLinks = (tag) => {
+//   const { rows } = await client.query(`
+//     SELECT 
+//   `);
+// };
+
+// // export
+// module.exports = {
+//   client,
+//   insertLink,
+//   insertTag,
+//   insertLinkTag,
+// };
+
+export default {
+  ...require('./links'),
+  ...require('./tags'),
+  ...require('./link_tags'),
 };
--- db/init_db.js
+++ db/init_db.js
@@ -1,10 +1,8 @@
 // code to build and initialize DB goes here
-const {
-  client,
-  insertLink,
-  insertTag,
-  insertLinkTag,
-} = require('./index');
+const { client } = require('./client');
+const { createLink } = require('/links');
+const { createTags } = require('./tags');
+const { createLinkTag } = require('./link_tags');
 
 async function buildTables() {
   try {
@@ -12,7 +10,7 @@ async function buildTables() {
 
     // drop tables in correct order
     await client.query(`
-      DROP TABLE IF EXISTS links_tags;
+      DROP TABLE IF EXISTS link_tags;
       DROP TABLE IF EXISTS tags;
       DROP TABLE IF EXISTS links; 
     `)
@@ -21,7 +19,7 @@ async function buildTables() {
     await client.query(`
       CREATE TABLE links (
         id SERIAL PRIMARY KEY,
-        link_name VARCHAR(255) NOT NULL,
+        link_name VARCHAR(255) NOT NULL UNIQUE,
         click_count BIGINT,
         comment VARCHAR(255),
         date DATE NOT NULL DEFAULT CURRENT_DATE
@@ -31,10 +29,12 @@ async function buildTables() {
         id SERIAL PRIMARY KEY,
         tag_name VARCHAR(255) NOT NULL
       );
+      
       CREATE TABLE links_tags (
         id SERIAL PRIMARY KEY,
-        "linksId" INT REFERENCES links("id") ON DELETE CASCADE NOT NULL,
-        "tagsId" INT REFERENCES tags("id") ON DELETE CASCADE NOT NULL
+        "linksId" INT REFERENCES links(id) ON DELETE CASCADE NOT NULL,
+        "tagsId" INT REFERENCES tags(id) ON DELETE CASCADE NOT NULL,
+        UNIQUE ("linksId", "tagsId")
       );
     `);
 
@@ -45,9 +45,29 @@ async function buildTables() {
 
 async function populateInitialData() {
   try {
-    const linksId = await insertLink('www.espn.com');
-    const tagsId = await insertTag('sports');
-    await insertLinkTag(linksId, tagsId);
+    await createLink({
+      linkName: 'www.outschool.com',
+      clickCount: 0,
+      comment: 'Keep kids ages 3-18 engaged with thousands of classes and camps on the topics they are most passionate about!',
+      tags: ['online', 'learning', 'school']
+    });
+
+    await createLink({
+      linkName: 'www.noredink.com',
+      clickCount: 0,
+      comment: 'Build better writers in person or remotely.',
+      tags: ['grammar', 'english', 'writing']
+    });
+
+    await createLink({
+      linkName: 'www.edx.com',
+      clickCount: 0,
+      comment: 'Free Online Courses by Harvard, MIT, and More.',
+      tags: ['online', 'education', 'learning']
+    });
+
+    await createTags(['high school', 'K-12']);
+    await createLinkTag(1, 4);
 
   } catch (error) {
     throw error;
